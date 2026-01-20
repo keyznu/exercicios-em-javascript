99304e38d5b662e06f4e63ecf10a1387a322f109
keyznu <samela.939.santos@gmail.com>
2026-01-20 19:16:07 -0300

    feat: adiciona nova atividade em JavaScript

---
+++ Java/script.js
@@ -0,0 +1,37 @@
+
+// questão 1
+// let num1 = 7;
+// let num2 = 3;
+// console.log("soma: " + (num1+num2));
+// console.log("subtração: " + (num1-num2));
+// console.log("multplicação: " + (num1*num2));
+// console.log("divisão: " + (num1/num2));
+
+//  questão 2
+// let base = 2;
+// let altura = 10;
+// console.log("multiplicação: " + (base*altura));
+
+
+// questão 3
+// let real = 50.75;
+// let dolar = 5.0;
+// console.log("divisão: " + (real/dolar));
+
+// questão 4
+// let number1 = 4;
+// let number2 = 3;
+// console.log("potenciação: " + (number1**number2))
+
+// questão 5
+// let nota1 = 6;
+// let nota2 = 8;
+// let nota3 = 10;
+// let nota4 = 5;
+// let media = 4;
+// console.log("média: " + (nota1+nota2+nota3+nota4/media))
+
+// questão 6
+let celcius = 25
+let fah = (celcius * 9/5) + 32;
+console.log(fah);
