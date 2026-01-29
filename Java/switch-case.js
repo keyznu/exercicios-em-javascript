11337802e6a696bcf674853c23a1e5b79aee1950
keyznu <samela.939.santos@gmail.com>
2026-01-29 20:55:21 -0300

    feat: adiciona mais questões da atividade switch-case.

--- Java/switch-case.js
+++ Java/switch-case.js
@@ -1,9 +1,174 @@
 // Atividades
 // 
 // Questão 1: Dias da semana
-let dia = 2
-switch(dia){
-    case 1:{
-        console.log("domingo");
-    }break;
-}
+// let dia = 2
+// switch(dia){
+//     case 1:{
+//         console.log("domingo");
+//     }break;
+//     case 2:{
+//         console.log("segunda");
+//     }break;
+//     case 3:{
+//         console.log("terça");
+//     }break;
+//     case 4:{
+//         console.log("quarta");
+//     }break;
+//     case 5:{
+//         console.log("quinta");
+//     }break;
+//     case 6:{
+//         console.log("sexta");
+//     }break;
+//     case 6:{
+//         console.log("sábado");
+//     }break;
+//     default:{
+//         console.log("não é um dia da semana");
+//     }break;
+// }
+
+// Questão 2: Meses do ano e estações
+// let mês = "abril"
+// switch(mês){
+//     case "janeiro":{
+//         console.log("verão");
+//     }break;
+//     case "fevereiro":{
+//         console.log("verão");
+//     }break;
+//     case "março":{
+//         console.log("outono");
+//     }break;
+//     case "abril":{
+//         console.log("outono");
+//     }break;
+//     case "maio":{
+//         console.log("outono");
+//     }break;
+//     case "junho":{
+//         console.log("inverno");
+//     }break;
+//     case "julho":{
+//         console.log("inverno");
+//     }break;
+//     case "agosto":{
+//         console.log("inverno");
+//     }break;
+//     case "setembro":{
+//         console.log("primavera");
+//     }break;
+//     case "outubro":{
+//         console.log("primavera");
+//     }break;
+//     case "novembro":{
+//         console.log("primavera");
+//     }break;
+//     case "dezembro":{
+//         console.log("verão");
+//     }break;
+//     default:{
+//         console.log("não é um mês do ano.");
+//     }break;
+// }
+// OBS: o case pode ser colocado um acima do outro para ficar performatico caso a resposta em console se repita em ambos.
+
+// // Questão 3: Calculadora
+// let calculo = "+"
+// let num1 = 2
+// let num2 = 10
+// switch(calculo){
+//     case "+":{
+//         console.log(num1+num2)
+//     }break;
+//     case "-":{
+//         console.log(num1-num2)
+//     }break;
+//     case "*":{
+//         console.log(num1*num2)
+//     }break;
+//     case "/":{
+//         console.log(num1/num2)
+//     }break;
+//     default:{
+//         console.log("Não é uma operação.")
+//     }break;
+// }
+
+// Questão 4: Sistema de Notas
+// let nota = "A"
+// switch(nota){
+//     case "A":{
+//         console.log("Excelente");
+//     }break;
+//     case "B":{
+//         console.log("Bom");
+//     }break;
+//     case "C":{
+//         console.log("Regular");
+//     }break;
+//     case "D":{
+//         console.log("Ruim");
+//     }break;
+//     case "F":{
+//         console.log("Reprovado");
+//     }break;
+//     default:{
+//         console.log("Nota não identificada pelo sistema.")
+//     }break;
+// }
+
+// Questão 5: Menu de lanchonete
+// let menu = 3
+// switch(menu){
+//     case 1:{
+//         console.log("X-Burguer: R$35,00")
+//     }break;
+//     case 2:{
+//         console.log("X-Salada: R$25,00")
+//     }break;
+//     case 3:{
+//         console.log("Batata-Frita: R$12,00")
+//     }break;
+//     case 4:{
+//         console.log("Refrigerante: R$10,00")
+//     }break;
+//     default:{
+//         console.log("Não é um ítem que se encontra no menu.")
+//     }break;
+// }
+
+// Questão 6: Verificador de Vogal
+// let letra = "D"
+// switch(letra){
+//     case "A":
+//     case "E":
+//     case "I":
+//     case "O":
+//     case "U":{
+//         console.log("É uma vogal");
+//     }break;
+//     default:{
+//          console.log("Consoante ou outro caractere.")
+//    }break;
+// }
+
+// Questão 7: Plano de Assinatura
+// let planos = "Ouro"
+// switch(planos){
+//     case "Bronze":{
+//         console.log("🟤 Plano Bronze: acesso básico aos recursos principais e suporte via e-mail.")
+//     }break;
+//     case "Prata":{
+//         console.log("⚪ Plano Prata: acesso a recursos avançados e suporte em horário comercial.")
+//     }break;
+//     case "Ouro":{
+//         console.log("🌟 Plano Ouro: acesso ilimitado a todos os recursos premium e suporte prioritário 24h.")
+//     }break;
+//     default:{
+//         console.log("Plano não encontrado.")
+//     }break;
+// }
+
+// Questão 8: Conversor de Moedas
