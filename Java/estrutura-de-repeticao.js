dd9e9221160dabf10f23c11cdd315f4694d34a44
keyznu <samela.939.santos@gmail.com>
2026-02-11 20:55:22 -0300

    feat: adiciona questoes

--- Java/estrutura-de-repeticao.js
+++ Java/estrutura-de-repeticao.js
@@ -1,6 +1,6 @@
 // 
 
-// ATIVIDADE
+// ATIVIDADE - FOR
 // Questão 1: Imprimir números de 1 a 20
 // for (let i = 1; i <= 20; ++i){
 //     console.log(i);
@@ -15,8 +15,51 @@
 // for (let i = 1; i <=15; i += 2){
 //     console.log(i);
 // }
+//ou
+// for (let i = 1; i <= 15; i++){
+//     if (i % 2 == 1) console.log(i)
+// }
 
 // Questão 4: Tabuada do 7
-for (let i = 1; i <= 10; i++){
-    console.log(`7 x ${i} = ${7 * i}`);
-}
+// for (let i = 1; i <= 10; i++){
+//     console.log(`7 x ${i} = ${7 * i}`);
+// }
+
+// Questão 5: Calcular soma de números de 1 a 10 e exibir resultado
+// let soma = 0
+// for (let i = 1; i <= 10; i++){
+//     soma += i;
+// }
+// console.log(soma)
+
+// Questão 6: Loop de palavra (letras separadas)
+// let palavra = "JavaScript";
+// for (let i = 0; i < palavra.length; i++){
+//     console.log(palavra[i])
+// }
+
+// Questão 7: Imprima os múltiplos de 5 entre 0 e 50.
+// for (let i = 0; i <= 50; i += 5){
+//     console.log(i)
+// }
+
+// Questão 8: soma de apenas os números pares de 1 a 20.
+// let soma = 0
+// for (let i = 2; i <= 20; i +=2){
+//     soma +=i
+// }
+// console.log(soma)
+
+// Questão 9: Quadrado de cada número de 1 a 10.
+// for (let i = 1; i <= 10; i++){
+//     console.log(i*i)
+// }
+
+// Questão 10: imprimir 10 *
+// let resultado = "";
+// for (let i = 1; i <= 10; i++){
+//    resultado += "*";
+// }console.log(resultado)
+
+// ATIVIDADE WHILE
+// Questão 11: Contador de 1 e para em 10
