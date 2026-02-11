4fec5430c22e5c698ec0ac3029a149259458d25e
keyznu <samela.939.santos@gmail.com>
2026-02-10 21:01:45 -0300

    feat: adiciona atividade e questoes

--- Java/estrutura-de-repeticao.js
+++ Java/estrutura-de-repeticao.js
@@ -1,6 +1,22 @@
-let i = 0;
-for (i = 0; i < 5; i++){
-    console.log(`Valor de i: ${i}`);
+// 
+
+// ATIVIDADE
+// Questão 1: Imprimir números de 1 a 20
+// for (let i = 1; i <= 20; ++i){
+//     console.log(i);
+// }
+
+// Questão 2: Imprimir números de 10 a 1 (decrescente)
+// for (let i = 10; i >= 1; --i){
+//     console.log(i);
+// }
+
+// Questão 3: Números impares de 1 a 15
+// for (let i = 1; i <=15; i += 2){
+//     console.log(i);
+// }
+
+// Questão 4: Tabuada do 7
+for (let i = 1; i <= 10; i++){
+    console.log(`7 x ${i} = ${7 * i}`);
 }
-i = i + 3;
-console.log(`Valor de i: ${i}`);
