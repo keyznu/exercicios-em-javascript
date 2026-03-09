81ca10002ff89825868ff8bbec45822ed53092fa
keyznu <samela.939.santos@gmail.com>
2026-03-09 18:52:45 -0300

    feat: adiciona alguma coisa ao assunto novo

---
+++ Java/assunto-novo.js
@@ -0,0 +1,23 @@
+// Questão 1
+// let base = prompt("informe o valor da base:");
+// let altura = prompt("informe o valor da altura:");
+// let result = 0;
+// result = base * altura
+// alert (`a area desse retângulo é ${result}cm`);
+
+// questão 2: média aritmetica
+// let nota1 = parseFloat(prompt("Digite a nota 1:"));
+// let nota2 = parseFloat(prompt("Digite a nota 2:"));
+// let nota3 = parseFloat(prompt("Digite a nota 3:"));
+// let nota4 = parseFloat(prompt("Digite a nota 4:"));
+// let mediaAritimetica = (nota1 + nota2 + nota3 + nota4 /4);
+// alert (`a média é: ${mediaaritmética}`);
+
+// questão 3: calcular imc peso*altura ao quadrado
+// let peso = parseFloat(prompt("Digite o peso:"));
+// let altura = parseFloat(prompt("Digite a altura:"));
+// let result = 0;
+// result = (peso/altura**2);
+// alert (`O cálculo IMC é igual a: ${result}`);
+
+// Questão 4: verificar se um numero é igual ou menor que zero.
