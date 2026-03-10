dccb5961f1cf12210c1879bb2b98caa19dd624b7
keyznu <samela.939.santos@gmail.com>
2026-03-09 21:04:25 -0300

    feat: adiciona correcoes para assunto novo

--- Java/assunto-novo.js
+++ Java/assunto-novo.js
@@ -20,4 +20,63 @@
 // result = (peso/altura**2);
 // alert (`O cálculo IMC é igual a: ${result}`);
 
-// Questão 4: verificar se um numero é igual ou menor que zero.
+// CORREÇÕES (A partir daqui é só "???"")
+// Questão 14
+// let nivelacess = prompt("Informe o seu nível de acesso: ");
+// switch(nivelacess){
+//     case "admin":{
+//         alert("Olá administrador!")
+//         break;
+//     }
+//     case "editor":{
+//         alert("Olá editor!")
+//         break;
+//     }
+//     case "visitante":{
+//         alert("Olá visitante!")
+//         break;
+//     }
+//     default:{
+//         alert("Quem é você?")
+//         break;
+//     }
+// }
+
+// Questão 15
+// while(true){
+//     let nota1 = parseFloat(prompt("informe a primeira nota: "));
+//     let nota2 = parseFloat(prompt("informe a segunda nota: "));
+//     let nota3 = parseFloat(prompt("informe a terceira nota: "));
+//     let nota4 = parseFloat(prompt("informe a quarta nota: "));
+//     let nota5 = parseFloat(prompt("informe a quinta nota: "));
+
+//     let media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+
+//     alert(`A média da turma é: ${media}`);
+//     break;
+// }
+
+// Questão 16
+// let distancia = parseFloat(prompt("Informe a distância percorrida: "));
+// let consumomedio = parseFloat(prompt("Informe o consumo médio do carro: "));
+// let combustivel = 5.80;
+// let custototal = consumomedio * combustivel / distancia;
+// alert(`O custo total da distância percorrida é de R$ ${custototal.toFixed(2)}`);
+
+// Questão 17
+// let num = 0.0;
+// do{
+//     num = parseFloat(prompt("Informe um número: "));
+// } while (num<100);
+
+// Questão 18
+// let menuop = 0;
+// do{
+//     menuop = parseFloat(prompt("Escolha uma opção: 1-Café, 2-Chá, 3-Sair"));
+// } while (menuop != 3);
+
+// Questão 19
+// let soma = 0;
+// do{
+//     soma += parseFloat(prompt("Digite um n-umero para efetuar a soma: "));
+// } while (soma === 0);
