13db3ac19d442591454bf93c0db131bd011b29f9
keyznu <samela.939.santos@gmail.com>
2026-03-12 18:53:54 -0300

    feat: adiciona lista de questoes para função

---
+++ Java/funcao-questoes.js
@@ -0,0 +1,24 @@
+// Questão 2: Calculadora de Bolo - Crie uma função que receba o preço do quilo de um bolo e o peso solicitado.
+// A função deve retornar o valor total a ser pago.
+// function calcularValorBolo(precoQuilo, peso) {
+//     return precoQuilo * peso;
+// }
+// let preco = parseFloat(prompt("Digite o preço do quilo do bolo:"));
+// let peso = parseFloat(prompt("Digite o peso do bolo em quilos:"));
+// let valorTotal = calcularValorBolo(preco, peso);
+// alert("Valor total: R$" + valorTotal.toFixed(2)); //to.Fixed(2) serve para adicionar o ",00"
+
+// Questão 3: Conversor de medida - Crie uma função que receba um valor em gramas e retorne o equivalente em quilos
+// (ex: 1500 -> 1.5kg)
+// function converterMedida(PesoGramas, PesoQuilos){
+//     return PesoGramas / 1000;
+// }
+// let gramas = parseFloat(prompt("Digite o peso em gramas: "));
+// let total = gramas / 1000;
+// alert(`Peso em quilos = ${total}`);
+
+// Questão 4: Verificador de estoque - Crie uma função que receba a quantidadeAtual e quantidadeMinima.
+// Se a atual for menor que a mínima, retorne "Necessário repor estoque", caso contrario, "Estoque OK".
+// function verificarEstoque(quantidadeAtual, quantidadeMinima)(
+    
+// )
---
+++ Java/funcao.js
@@ -0,0 +1,16 @@
+function modulacao(a,b){
+    return a % b;
+}
+
+function convertToFahrenheit(C){
+    return (C * 9) / 5 + 32;
+}
+
+const soma1 = (a,b) => a + b;
+const subtracao1 = (a,b) => a - b;
+const multiplicacao1 = (a,b) => a * b;
+const divisao1 = (a,b) => a / b;
+const expoente1 = (a,b) => a ** b;
+const modulacao1 = (a,b) => a % b;
+
+const convertToFahrenheit = (c) => (c * 9) / 5 + 32;
