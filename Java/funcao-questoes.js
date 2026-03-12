d7433ff077680a713cb70d2e86a145553f16bebb
keyznu <samela.939.santos@gmail.com>
2026-03-12 20:40:10 -0300

    feat: adiciona questoes de função

--- Java/funcao-questoes.js
+++ Java/funcao-questoes.js
@@ -19,6 +19,43 @@
 
 // Questão 4: Verificador de estoque - Crie uma função que receba a quantidadeAtual e quantidadeMinima.
 // Se a atual for menor que a mínima, retorne "Necessário repor estoque", caso contrario, "Estoque OK".
-// function verificarEstoque(quantidadeAtual, quantidadeMinima)(
-    
-// )
+// function verificarEstoque(quantidadeAtual, quantidadeMinima){
+//     if(quantidadeAtual < quantidadeMinima){
+//         return "Necessário repor estoque";
+//     } else{
+//         return "Estoque OK";
+//     }
+// }
+// let atual = parseFloat(prompt("Digite a quantidade atual em estoque: "));
+// let minimo = parseFloat(prompt("Digite a quantidade mínima necessária: "));
+// let resultado = verificarEstoque(atual, minimo);
+// alert(resultado);
+
+// Questão 5: Desconto de Fidelidade - Crie uma arrow function que receba um valor e retorne esse
+// valor com 10% de desconto (tente fazer em apenas uma linha)
+// let descontoFidelidade = valor => valor * 0.9;
+// let preco = parseFloat(prompt("Digite o valor do produto:"));
+// let precoComDesconto = descontoFidelidade(preco);
+// alert("Preço com desconto: R$" + precoComDesconto.toFixed(2));
+
+// Questão 6: Verificador de Paridade - Crie uma arrow funcion que receba um número
+// e retorne true se ele for par e false se for ímpar.
+// let verificarParidade = num => num % 2 === 0;
+// let num = parseFloat(prompt("Digite um número para verificar paridade:"));
+// let resultado = verificarParidade(num);
+// if (resultado) {
+//     alert("O número é par");
+// } else {
+//     alert("O número é ímpar");
+// }
+
+// Questão 7: Calculo de Área - Crie uma arrow function que receba a base e a altura de uma forma geometrica
+// e retorne a área ($Base /times Altura$)
+// let calcularArea = (base, altura) => base * altura;
+// let base = parseFloat(prompt("Informe o valor da base: "));
+// let altura = parseFloat(prompt("Informe o valor da altura: "));
+// let resultado = calcularArea(base, altura);
+// alert(`A área deste espaço é: ${resultado}`);
+
+// Questão 8: Filtro de texto - Crie uma arrow function que receba uma string e
+// retorne a mesma string em letras maiusculas (Use toUpperCase.)
