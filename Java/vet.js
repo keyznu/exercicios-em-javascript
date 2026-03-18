282f59cc52e8c97a7d3acca859b9593bdae70ead
keyznu <samela.939.santos@gmail.com>
2026-03-18 20:34:37 -0300

    feat: adiciona questoes de vet

---
+++ Java/vet.js
@@ -0,0 +1,63 @@
+// Questão 1: - Declare e defina um Vetor com 5 índices
+// let vet1 = [1,2,3,4,5];
+// Questão 2: Utilize o método .pop() para remover o último ítem
+// vet1.pop();
+// Questão 3: Utilize o método .unshift() para adicionar um ítem a frente da linha
+// vet1.unshift();
+
+// ATIVIDADE
+
+// Questão 1: Crie um vetor com as cores vermelho, verde e azul e imprima o primeiro item
+// let vet = ["vermelho",  "verde", "azul"];
+// alert(vet[0])
+
+// Questão 2: Adicione a cor amarelo ao final de um vetor de cores usando o método push
+// let cores = ["vermelho", "azul", "verde"];
+// cores.push("amarelo"); 
+
+// Questão 3: Remova o último elemento de um vetor de nomes e exiba o nome removido.
+// let nomes = ["Kauê", "Adrian", "Jorge", "Bruna"];
+// let removido = nomes.pop();
+// alert(removido);
+
+// Questão 4: Verifique qual é o tamanho total de um vetor contendo dez números inteiros.
+// let numeros = [1,2,3,4,5,6,7,8,9,10];
+// alert("Tamanho do vetor: " + numeros.length);
+
+// Questão 5: Acesse o índice de um vetor contendo dez números inteiros.
+// let numeros = [1,2,3,4,5,6,7,8,9,10];
+// alert(numeros[9]);
+
+// Questão 6: Crie um vetor com cinco números e calcule a soma do primeiro com o último elemento.
+// let numeros = [2, 4, 6, 8, 10];
+// let soma = numeros[0] + numeros[4];
+// alert("Soma do primeiro com o último: " + soma);
+
+// Questão 7: Use o método unshift para adicionar um novo sabor ao ínicio de um vetor de sorvetes.
+// let sorvetes = ["chocolate", "morango", "menta"];
+// sorvetes.unshift("maracuja");
+
+// Questão 8: Remova o primeiro elemento de um vetor de cidades e exiba o vetor resultante.
+// let cidades = ["Salvador", "Cajamar", "São Paulo"];
+// let removida = cidades.shift();
+// alert(removida)
+
+// Questão 9: Encontre o índice da palavra banana dentro de um vetor de frutas usando indexOf.
+// let frutas = ["maçã", "banana", "laranja", "uva"];
+// let indice = frutas.indexOf("banana");
+// alert("Índice da palavra banana: " + indice);
+
+// Questão 10: Crie um vetor com quatro animais e use o método reverse para inverter a ordem deles.
+// let animais = ["cachorro", "gato", "elefante", "leão"];
+// animais.reverse();
+// alert(animais);
+
+// Questão 11: Use um laço for para percorrer um vetor de idades e imprimir cada uma delas.
+
+// Questão 12: Crie um vetor de preços e use um loop para somar todos os valores contidos neles.
+
+// Questão 13: Dado um vetor de nomes, use um loop para contar quantos nomes possuem mais de 5 letras.
+
+// Questão 14: Percorra um vetor de números e imprima apenas os valores que forem maiores que 50.
+
+// Questão 15: Crie um scipt que identifique o maior valor dentro de um vetor de números inteiros.
